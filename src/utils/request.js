323cb55b94b34bfe2ce034fee6aac123d67f4f11
linhexi <2332019407@qq.com>
2025-12-02 20:05:15 +0800

    修改后端接口地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://129.204.4.160:8080/homework' 
+  baseURL: 'https://personal-project.cloud:8080/homework' 
 })
 export default request // 导出，其他组件能用到
